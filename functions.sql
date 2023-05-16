3204d8da808af2050dd418315f365eb1b65090ac
jovankit <jovankitanov34@gmail.com>
2023-05-16 23:15:45 +0200

    Insert functions

---
+++ functions.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE FUNCTION insert_team_function(p_result text, p_judgeId int, p_stadiumId int, p_seasonId int,
+                                                p_homeTeamId int, p_awayTeamId int, p_date date)
+    RETURNS VOID AS
+$$
+BEGIN
+    insert into matches(result, judgeid, stadiumid, seasonid, hometeam, awayteam, "Date")
+    values (p_result, p_judgeId, p_stadiumId, p_seasonId, p_homeTeamId, p_awayTeamId, p_date);
+END
+$$;
+
+CREATE OR REPLACE FUNCTION insert_ticket_function(p_stake text, p_return text, p_odd text, p_state text, p_userid int)
+    RETURNS VOID AS
+$$
+BEGIN
+    insert into tiket(stake, "Return", odd, state, userid)
+    values (p_stake, p_return, p_odd, p_state, p_userid);
+END
+$$;
+
+CREATE OR REPLACE FUNCTION insert_bet_function(p_coefficiten text, p_state text, p_match int, p_combination int)
+    RETURNS VOID AS
+$$
+BEGIN
+    insert into tiketbet(Coefficient, State, MatchesId, BettingCombinationsId)
+    values (p_coefficiten, p_state, p_match, p_combination);
+END
+$$;
+
+CREATE OR REPLACE FUNCTION insert_user_function(p_first_name text, p_last_name text, p_date_of_birth date, p_email text,
+                                                p_location_id int,
+                                                p_gender_id int, p_username text, p_password text, p_locationId int,
+                                                p_roleId int, p_cardIssue int, p_expire date)
+    RETURNS VOID AS
+$$
+BEGIN
+    insert into person(first_name, last_name, date_of_birth, email, location_id, gender_id)
+    values (p_first_name, p_last_name, p_date_of_birth, p_email, p_location_id, p_gender_id);
+    insert into debitcard(issue, dateofexpire, "Column")
+    values (p_cardIssue, p_expire, 0);
+    insert into bettingcard(totalmoney, debitcardid)
+    values (0, (select id from debitcard where issue = p_cardIssue));
+    insert into "User"(username, password, person_id, locationid, roleid, "Betting CardId")
+    VALUES (p_username, p_password, (select id from person where last_name = p_last_name and first_name = p_first_name),
+            p_locationId, p_roleId,
+            (select id from bettingcard where debitcardid = (select id from debitcard where issue = p_cardIssue)));
+END
+$$;
