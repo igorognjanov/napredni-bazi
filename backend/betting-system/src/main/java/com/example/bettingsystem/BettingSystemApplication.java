64192429e2b29ada3283626258eddb2e1173b014
Igor Ognjanov <igorognjanov123@gmail.com>
2023-06-11 14:45:36 +0200

    add backend app

---
+++ backend/betting-system/src/main/java/com/example/bettingsystem/BettingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.bettingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettingSystemApplication.class, args);
+	}
+
+}
